41b09075f4df66ed4a0fca6905125802c33af7e0
Susanne Mäkelä <susamakela@Susanne-MacBook-Pro.local>
2023-04-19 13:25:12 +0300

    teht 11.5 another try

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
+/* just adding a comment for testing purposes */
 if (typeof window !== 'undefined') {
   ReactDOM.render(<App />, document.getElementById('app'))
 }
